ccb28530f74576b1c05d4de68200b2d73e37e617
Felipe <felipemelogomes36@gmail.com>
2022-07-14 23:23:54 -0300

    ProjetoBurger

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <footer id="footer">
+        <p>Make Your Burguer &copy; 2022</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top:4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
+
+    p {
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        margin-right: 7%;
+    }
+</style>
